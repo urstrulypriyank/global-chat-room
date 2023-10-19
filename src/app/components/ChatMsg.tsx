f0638ecb5a3403a915a6eacc83dc361ec231f74c
priyank rai <type2email@gmail.com>
2023-10-19 15:27:33 +0530

    bug fix

--- src/app/components/ChatMsg.tsx
+++ src/app/components/ChatMsg.tsx
@@ -12,7 +12,7 @@ const ChatMsg = ({ message }: Props) => {
     <p className=" items-center mb-2 font-semibold my-4">
       <span className="bg-[#005c4b] text-white p-1 rounded-lg">
         <span className="text-sm text-black tracking-widest">
-          {message.name}
+          {message.name}:
         </span>
         <span className="ml-2 text-sm flex-wrap">{message.message}</span>
       </span>
